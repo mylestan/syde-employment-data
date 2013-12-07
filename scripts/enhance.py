aa2288b7ab488d29a2d7fe24492718d2bbc94829
Myles Tan <mylestan7@gmail.com>
2013-12-07 14:11:34 -0500

    enhance script is reading files now :)

--- scripts/enhance.py
+++ scripts/enhance.py
@@ -6,8 +6,18 @@
 
 # Imports
 import os
+import csv
+import glob
 
 # we run it for each of the files:
 for filename in glob.glob("/Users/mylestan/Git/syde-employment-data/raw/*.csv"):
-	with open("../raw/" + filename):
-		# tbc
+	print '\n\n\n' + filename + '\n\n\n'
+	with open(filename) as csvfile:
+		csvfile.readline() # soft titles
+		keys = csvfile.readline().split(",")
+		csvfile.readline() # explanation text
+
+		csvreader = csv.DictReader(csvfile, fieldnames = keys)
+		for obj in csvreader:
+			print obj
+			print "\n\n\n"
